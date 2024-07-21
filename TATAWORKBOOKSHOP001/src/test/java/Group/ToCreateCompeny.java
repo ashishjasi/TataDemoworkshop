21be549242b674055c15e55d97029af3fadc690d
arjune221105 <shakti221105@gmail.com>
2024-07-20 20:40:25 -0700

    Compeny Add

---
+++ TATAWORKBOOKSHOP001/src/test/java/Group/ToCreateCompeny.java
@@ -0,0 +1,9 @@
+package Group;
+
+public class ToCreateCompeny {
+
+	public static void main(String[] args) {
+		System.out.println("TCS Main Compeny");
+		System.out.println("TCS Compeny Successfully Running in the Market");
+	}
+}
