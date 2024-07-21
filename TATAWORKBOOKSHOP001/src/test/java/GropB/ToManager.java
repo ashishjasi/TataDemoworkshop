8abed7a50005e215905143c8c00f8e6747ba225d
Nilamber <nilambersingh@gmail.com>
2024-07-21 09:18:07 +0530

    add Manager

---
+++ TATAWORKBOOKSHOP001/src/test/java/GropB/ToManager.java
@@ -0,0 +1,10 @@
+package GropB;
+
+public class ToManager {
+	public static void main(String[] args) {
+		System.out.println("Manager");
+		System.out.println("Employee");
+		System.out.println("Succesfully");
+	}
+
+}
